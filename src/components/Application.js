6184086932701c51c772a63517de4cc64b1d81a5
Lewis Lee <lewisforbusiness@gmail.com>
2021-11-08 18:01:35 -0500

    update on bug with Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -20,9 +20,9 @@ export default function Application(props) {
     const interview = getInterview(state, appointment && appointment.interview)
     return (
       <Appointment 
-        key={appointment.id}
-        id={appointment.id}
-        time={appointment.time}
+        key={appointment && appointment.id}
+        id={appointment && appointment.id}
+        time={appointment && appointment.time}
         interview={interview}
         interviewers={interviewers}
         bookInterview={bookInterview}
